b0cd591f77c71722a08efe7aca93a69053dc32b9
David Greis <dgreis@ischool.berkeley.edu>
2013-11-08 21:22:41 -0800

    Added data, psuedo-code for sentenceLister fn, and added .DS_Store to .gitignore

---
+++ sentenceLister.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os
+
+def sentenceLister(file):
+	'''
+	1. Open file
+	2. Split text on periods BUT ALSO get the id of article that the sentence came from
+	3. Return dict object
+			{ 'id': id
+			  'sents': [sents]
+			}
+	'''
+
